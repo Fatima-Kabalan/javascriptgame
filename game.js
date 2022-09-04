208d51d33bd048706a8b5f807535d4ef381ee8b6
FatimaKabalan <kabalan.fatima01@gmail.com>
2022-09-05 01:42:19 +0300

    moving the block

--- game.js
+++ game.js
@@ -1,4 +1,60 @@
 document.addEventListener("DOMContentLoaded",function(){
+    var start = document.getElementById("start")
+    start.onmouseover = function() {mouseOver()};
+    function mouseOver() {
+        start.style.color = "blue";
+        boundary.textContent=("game start");
+    }
+
+    start.onmouseout= function handleMouseOut(){
+        start.style.color = 'black';
+    };
+
+    start.onmousedown = function(event) {
+    // (1) prepare to moving: make absolute and on top by z-index
+    start.style.position = 'absolute';
+    start.style.zIndex = 1000;
+
+    document.body.append(start);
+
+    // centers the box at (pageX, pageY) coordinates
+    function moveAt(pageX, pageY) {
+    start.style.left = pageX - start.offsetWidth / 2 + 'px';
+    start.style.top = pageY - start.offsetHeight / 2 + 'px';
+    }
+
+    // move our absolutely positioned ball under the pointer
+    moveAt(event.pageX, event.pageY);
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+
+    // (2) move the box on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // (3) drop the box, remove unneeded handlers
+    start.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        start.onmouseup = null;
+    };
+
+    };
+    start.ondragstart = function() {
+        return false;
+    };
+
+    var boundaries = document.getElementsByClassName("boundary")
+    for (var i = 0 ; i < boundaries.length;i++){
+        boundaries[i].addEventListener("mouseover", mouseOver );
+
+    }
+
+
+
+
+
+
 
 
     var boundaries = document.getElementsByClassName("boundary")
@@ -7,6 +63,7 @@ document.addEventListener("DOMContentLoaded",function(){
         for (var i = 0; i < boundaries.length; i++) {
             boundaries[i].addEventListener("mouseover",mouseOver);
         }
+
         function mouseOver(){
             for (var i = 0 ; i < boundaries.length;i++){
                 boundaries[i].style.backgroundColor="red";
